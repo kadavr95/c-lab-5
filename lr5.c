0a21340f4cc448a2521e9a2018b6e82cda984d9f
Yaskovich Dmitry <kadavr95@outlook.com>
2012-10-26 23:01:00 +0300

    changed input prompt format, added additional info on results output

--- lr5.c
+++ lr5.c
@@ -8,9 +8,17 @@ int main(void){
   printf("Lab 5. Variant 3\n\n");
   for (i=1; i <= n3 ; i++) {
 	if ((i==n1) || (i==n2) || (i==n3)) {
-	  printf("Number:%d\n",i);
+	  printf("Number:       %d\n",i);
 	  printf("No correction:%.30e\n",s2);
 	  printf("Correction:   %.30e\n",s);
+	  if ((s2-s)>0)
+		printf("Comparison:   Sum with no correction is bigger than sum with correction\n");
+	  if ((s2-s)<0) {
+		printf("Comparison:   Sum with correction is bigger than sum with no correction\n");
+	  }
+	  if ((s2-s)==0) {
+		printf("Comparison:   Sum with no correction is equal to sum with correction\n");
+	  }
 	  printf("Difference:   %.30e\n\n",fabs(s2-s));
 	}
 	f=exp(sqrt(3*i/2));
