b95cf181f767ca4c837008fcefd8916dc84c0760
Yaskovich Dmitry <kadavr95@outlook.com>
2012-10-27 23:04:00 +0300

    minor fixes, added comments

--- lr5.c
+++ lr5.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-#include <limits.h>
+#include <stdio.h>                                                              //подключение библиотек содержащих функции ввода, вывода и математических  функций
 #include <math.h>
 
 int main(void){
-  double s=0.0,scor=0.0,cor=0.0,fcor,f,s2=0.0;
-  int n1=1000,n2=1050,n3=1100,i;
-  printf("Lab 5. Variant 3\n\n");
-  for (i=1; i <= n3 ; i++) {
-	f=exp(sqrt(3*i/2));
+  double s=0.0,scor=0.0,cor=0.0,fcor,f,s2=0.0;                                  //определение переменных и присваивание им необходимых значений
+  short int n1=1000,n2=1050,n3=1100,i;
+  printf("Yaskovich Dmitry (T01-01c). Lab 5. Variant 3\n\n");                   //Информация о программе
+  for (i=1; i <= n3 ; i++) {                                                    //цикл, проходящий по всем i в пределах заданных в условии
+	f=exp(sqrt(3*i/2));                                                         //суммирование по методу Кохена
 	fcor=f-cor;
 	scor=s+fcor;
 	cor=(scor-s)-fcor;
 	s=scor;
-	s2=s2+f;
-	if ((i==n1) || (i==n2) || (i==n3)) {
-	  printf("Number:       %d\n",i);
+	s2=s2+f;                                                                    //обычное суммирование
+	if ((i==n1) || (i==n2) || (i==n3)) {                                        //условие вывода результатов
+	  printf("Number:       %d\n",i);                                           //вывод результатов
 	  printf("No correction:%.30e\n",s2);
 	  printf("Correction:   %.30e\n",s);
 	  if ((s2-s)>0)
@@ -28,10 +27,10 @@ int main(void){
 	  printf("Difference:   %.30e\n\n",fabs(s2-s));
 	}
   }
-  printf("Dimini Inc.\n");
+  printf("Dimini Inc.\n");                                                      //Авторские права
   printf("For progressive future\n");
   printf("(c)2009-2012\n");
-  fflush(stdin);
+  fflush(stdin);                                                                //ожидание нажатия на клавишу для завершения программы
   getchar();
   return 0;
 }
